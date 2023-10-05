0764a42c35467e0ee766a1a46b0a791bb0d1e1fe
kberdos <kazuya_erdos@brown.edu>
2023-10-05 14:07:16 -0400

    firebaseconfig

--- components/util/makePlayer.ts
+++ components/util/makePlayer.ts
@@ -3,12 +3,12 @@ import { collection, doc, getDoc, setDoc, addDoc } from "firebase/firestore";
 import { Player } from "@/app/about/page";
 
 interface postPlayerProps {
-    name: String,
-    number: Number,
-    height: Number
+  name: String;
+  number: Number;
+  height: Number;
 }
 
-export async function postPlayer(player : Player) {
-    const docRef = await addDoc(collection(db, '/players'), player);
-    return docRef.id;
+export async function postPlayer(player: Player) {
+  const docRef = await addDoc(collection(db, "/players"), player);
+  return docRef.id;
 }
