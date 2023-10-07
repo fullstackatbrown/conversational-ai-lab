a402a21cb20ca34c5353b2a7fdc11d989cc572e4
Praccho Muna-McQuay <praccho@brown.edu>
2023-10-07 16:43:20 -0400

    Login page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { db } from "../../firebaseClient";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+}
