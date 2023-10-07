f542853c8b249695f7b2abcc41493c2293a1e966
Praccho Muna-McQuay <praccho@brown.edu>
2023-10-07 17:37:41 -0400

    Testing firebasedatabase

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,41 @@
 'use client'
 import { db } from "../../firebaseClient";
-import { collection, doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult, onAuthStateChanged } from "firebase/auth";
 import { firebaseApp } from "../../firebaseClient";
 
 export default function Login() {
+    const [uid, setUid] = useState('nothin');
     
     const auth = getAuth(firebaseApp);
     const provider = new GoogleAuthProvider();
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setUid(user.uid);
+        }
+    })
+
+    useEffect(async () => {
+        const ref = doc(db, 'users', uid);
+        setDoc(ref, {});
+    }, [uid])
 
     async function handleSignIn() {
         await signInWithRedirect(auth, provider);
         const result = await getRedirectResult(auth);
 
         if (result) {
+            const user = result.user;
+            setUid(user.uid);
             
         }
     }
 
     return (
         <div>
-            <button onClick={() => handleSignIn()}>Login</button>
+            <button onClick={() => handleSignIn()}>{uid}</button>
         </div>
     );
 }
