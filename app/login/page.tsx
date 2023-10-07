7872fc933cb2741eedbd741eabf54a7670e0cce2
Praccho Muna-McQuay <praccho@brown.edu>
2023-10-07 17:13:00 -0400

    Added google auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,26 @@
 import { db } from "../../firebaseClient";
 import { collection, doc, getDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import { firebaseApp } from "../../firebaseClient";
 
 export default function Login() {
+    
+    const auth = getAuth(firebaseApp);
+    const provider = new GoogleAuthProvider();
+
+    async function handleSignIn() {
+        await signInWithRedirect(auth, provider);
+        const result = await getRedirectResult(auth);
+
+        if (result) {
+            
+        }
+    }
+
     return (
         <div>
-            <h1>Login</h1>
+            <button onClick={() => handleSignIn()}>Login</button>
         </div>
     );
 }
