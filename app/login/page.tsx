d64df31c24ebeb2bf2ba869b7a07a8156adfb790
kberdos <kazuya_erdos@brown.edu>
2023-10-12 14:20:28 -0400

    dynamic routing for user profiles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,44 +1,68 @@
 'use client'
 import { db } from "../../firebaseClient";
-import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { collection, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult, onAuthStateChanged } from "firebase/auth";
 import { firebaseApp } from "../../firebaseClient";
+import { useRouter } from "next/navigation";
+
 
 export default function Login() {
-    const [uid, setUid] = useState('nothin');
+    const [uid, setUid] = useState<string | null>(null);
+    const [email, setEmail] = useState<string | null>(null);
 
     const auth = getAuth(firebaseApp);
     const provider = new GoogleAuthProvider();
 
-    onAuthStateChanged(auth, (user) => {
+    const router = useRouter();
+
+    onAuthStateChanged(auth, async (user) => {
         if (user) {
             setUid(user.uid);
+            setEmail(user.email);
+            await createUserDBEntry(user.uid);
+            console.log("User signed in");
         }
     })
 
-    useEffect(() => {
-        async function createUser(uid: string) {
-            const ref = doc(db, 'users', uid);
-            await setDoc(ref, {});
-        }
-        createUser(uid);
-    }, [uid])
-
-    async function handleSignIn() {
-        await signInWithRedirect(auth, provider);
-        const result = await getRedirectResult(auth);
-
-        if (result) {
-            const user = result.user;
-            setUid(user.uid);
-
+    async function createUserDBEntry(uid: string) {
+        const ref = doc(db, 'users', uid);
+        // only create the doc if it doesn't already exist exist
+        const docSnap = await getDoc(ref);
+        if (!docSnap.exists()) {
+            await setDoc(ref, {
+                email: email,
+                created: new Date(),
+                lastLogin: new Date()
+            })
+        } else {
+            await updateDoc(ref, {
+                lastLogin: new Date()
+            })
         }
     }
 
+    async function handleSignIn() {
+        await signInWithRedirect(auth, provider);
+        await getRedirectResult(auth);
+    }
+
+    async function handleSignOut() {
+        await auth.signOut();
+        setUid(null);
+        setEmail(null);
+    }
+
+    async function redirectProfile() {
+        // use next router to redirect to profile page
+        router.push("../profiles/" + uid)
+    }
+
     return (
-        <div>
-            <button onClick={() => handleSignIn()}>{uid}</button>
+        <div className="flex flex-col gap-5">
+            <button onClick={() => handleSignIn()}>{email ? "Welcome, " + email + "!" : "Sign in"}</button>
+            <button onClick={() => handleSignOut()} >Sign Out</button>
+            <button onClick={() => redirectProfile()}>Your Profile</button>
         </div>
     );
 }
