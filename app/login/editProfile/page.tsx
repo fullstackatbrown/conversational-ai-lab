538fdf0cd1b3833203724f30e230d49620292f88
kberdos <kazuya_erdos@brown.edu>
2023-10-12 21:13:40 -0400

    materialui

--- app/login/editProfile/page.tsx
+++ app/login/editProfile/page.tsx
@@ -2,35 +2,57 @@
 import { firebaseApp } from "@/firebaseClient";
 import { GoogleAuthProvider, getAuth, onAuthStateChanged } from "firebase/auth";
 import { get } from "http";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { UserData } from "../../../components/util/types";
 import { getUserData } from "@/components/util/userDBFunctions";
+import { TextField } from "@mui/material"
 
-const EditProfile = () => {
-    const [uid, setUid] = useState<string>("");
+const EditProfile = (props: { uid: string }) => {
     const [email, setEmail] = useState<string>("");
     const [userName, setUserName] = useState<string>("");
     const [bio, setBio] = useState<string>("");
 
-    const auth = getAuth(firebaseApp);
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            setUid(user.uid);
-            setEmail(user.email!);
-        }
-    })
-    if (uid) {
-        getUserData(uid).then((data) => {
-            console.log(data)
-            setUserName(data.userName);
+    useEffect(() => {
+        getUserData(props.uid).then((userData: UserData) => {
+            setUserName(userData.userName);
+            setEmail(userData.email)
+            setBio(userData.bio);
         })
-    }
-    return (<div>
-        <h1>Edit your Profile</h1>
-        <div>Email: {email}</div>
-        <div>Username: {userName}</div>
-    </div>)
+    }, [])
+
+    return (
+        <div>
+            <h1 className="my-5 font-serif text-center text-2xl">Edit your Profile</h1>
+            <div className="flex flex-col gap-2 items-center">
+                <div>
+                    <div>Email: {email}</div>
+                    <TextField
+                        id="outlined-basic"
+                        label="Username"
+                        variant="outlined"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                </div>
+            </div>
+        </div>)
 
 }
 
-export default EditProfile;
+const EditProfilePage = () => {
+    // get auth then call EditProfile
+    const [uid, setUid] = useState<string>("");
+    const auth = getAuth(firebaseApp);
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setUid(user.uid);
+        }
+    })
+    return (
+        <div>
+            {uid ? <EditProfile uid={uid} /> : <div>loading...</div>}
+        </div>
+    )
+}
+
+export default EditProfilePage;
