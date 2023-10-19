bf734920e201337390fd88533ed38ad40754d2cf
kberdos <kazuya_erdos@brown.edu>
2023-10-19 17:26:51 -0400

    bruh

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { ArrowLongRightIcon } from '@heroicons/react/24/outline'
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-[200vh]">
+    <div className="flex flex-col lg:h-[200vh] h-[200vh]">
       <div className="sticky top-0 w-full">
         <Navbar />
       </div>
@@ -22,7 +22,7 @@ export default function Home() {
           <div className='px-[70px] space-y-3'>
             <h1 className="lg:text-6xl text-4xl font-bold">About</h1>
             <div className="w-3/4 lg:text-2xl text-lg">Conversational AI based on large language models (LLMs), such as ChatGPT, has ignited widespread interest. The combination of LLMs with such utilities as LlamaIndex and LangChain has made it possible to create conversational interfaces to document sets, datasets, and structured data repositories. We aim to study and research in these areas.</div>
-            <div className="group flex flex-row w-1/5 gap-1 items-center">
+            <div className="group flex lg:w-1/3  w-auto gap-1 items-center">
               <p className="w-auto lg:text-xl font-bold text-lg group-hover:text-red-500 group-hover:underline underline-offset-4">Learn More</p>
               <span className="transition ease-in-out delay-50 mt-1 collapse group-hover:visible group-hover:translate-x-2">
                 <ArrowLongRightIcon className="block h-9 text-red-500" />
