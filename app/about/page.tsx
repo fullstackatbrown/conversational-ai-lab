51f03b1e711d1ea6508a511b31aaaf60b91e556b
kberdos <kazuya_erdos@brown.edu>
2023-10-27 13:34:56 -0400

    about simple page with image:

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,8 +18,18 @@ const Home = () => {
     return (
         <div>
             <div className="bg-[#9A9A9A] flex flex-col items-center justify-center w-full h-[200px]">
-                <div className="text-left w-full px-[150px]">
-                    <h1 className="lg:text-5xl text-2xl text-white">About Us</h1>
+                <div className="text-left w-full px-10 lg:px-[150px]">
+                    <h1 className="lg:text-5xl text-4xl text-white">About Us</h1>
+                </div>
+            </div>
+            <div className="lg:flex lg:gap-10">
+                <div className=" px-10 py-10 lg:px-20 lg:py-20 space-y-6 lg:w-1/2 w-full flex flex-col justify-center">
+                    <h1 className="transition-all duration-300 lg:text-4xl text-3xl font-bold">Who We Are</h1>
+                    <div className="transition-all duration-300 w-full lg:text-xl text-lg">Conversational AI based on large language models (LLMs), such as ChatGPT, has ignited widespread interest. The combination of LLMs with such utilities as LlamaIndex and LangChain has made it possible to create conversational interfaces to document sets, datasets, and structured data repositories. We aim to study and research in these areas.</div>
+                </div>
+                <div className="transition-all duration-300 scale-0 lg:scale-100 transform-scale-y-[-1]">
+                    <img src="/assets/wave.png" width={"800px"}
+                        height={"800px"} />
                 </div>
             </div>
         </div>
