e02b87ef495022d95336603f73159fd15501f03a
Praccho Muna-McQuay <praccho@brown.edu>
2023-10-28 15:46:56 -0400

    profile page w/ edit

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -3,24 +3,25 @@ import { firebaseApp } from '@/firebaseClient';
 import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { UserData, dummyUserData } from '@/components/util/types';
-import { getUserData } from '@/components/util/userDBFunctions';
+import { getUserData, updateUserData } from '@/components/util/userDBFunctions';
+import { TextField } from '@mui/material';
 import PageShell from '@/components/PageShell';
 
-function EditProfile(props: {pid: string, postData: Post, onSave: (post : Post) => void}) {
-    const postData = props.postData;
-    const [title, setTitle] = useState(postData.title);
-    const [body, setBody] = useState(postData.textContent);
+function EditProfile(props: {userData: UserData, onSave: (user : UserData) => void}) {
+    const userData = props.userData;
+    const [firstName, setFirstName] = useState(userData.firstName ? userData.firstName : "");
+    const [lastName, setLastName] = useState(userData.lastName ? userData.lastName : "");
+    const [about, setAbout] = useState(userData.bio);
 
     const handleSave = () => {
-        if (title != "") {
-            const updatedPost = {
-                ...postData,
-                title: title,
-                textContent: body
-            }
-
-            props.onSave(updatedPost);
+        const updatedPost = {
+            ...userData,
+            firstName: firstName,
+            lastName: lastName,
+            bio: about
         }
+
+        props.onSave(updatedPost);
     }
 
     return (
@@ -33,27 +34,46 @@ function EditProfile(props: {pid: string, postData: Post, onSave: (post : Post) 
 }
 
 function Profile(props: { profId: string, uid: string }) {
-    const [uid, setUid] = useState<string>(props.uid);
+    const uid = props.uid;
     const [profData, setProfData] = useState<UserData>(dummyUserData);
     const [editable, setEditable] = useState<boolean>(false);
     const [editMode, setEditMode] = useState<boolean>(false);
     const auth = getAuth(firebaseApp);
+
+    // edit states
+    const [userName, setUserName] = useState<string>("");
+    const [firstName, setFirstName] = useState<string>("");
+    const [lastName, setLastName] = useState<string>("");
+    const [bio, setBio] = useState<string>("");
     
 
     useEffect(() => {
         getUserData(props.profId).then((data : UserData) => {
             setProfData(data);
+            setUserName(data.userName);
+            data.firstName ? setFirstName(data.firstName) : null;
+            data.lastName ? setLastName(data.lastName) : null;
+            setBio(data.bio)
             if (data.uid == props.uid) {
-                console.log(data.uid)
                 setEditable(true);
             }
         });
     },[uid]);
 
-    // const handleSave = (newPost: Post) => {
-    //     setEditMode(false);
-    //     updatePost(props.pid, newPost);
-    // }
+    const handleSave = () => {
+        setEditMode(false);
+        const updatedUser = {
+            ...profData,
+            firstName: firstName,
+            lastName: lastName,
+            bio: bio
+        }
+        updateUserData(updatedUser);
+    }
+
+    const handleEdit = () => {
+        setEditMode(true);
+    }
 
     return (
         <>
@@ -62,19 +82,61 @@ function Profile(props: { profId: string, uid: string }) {
         <div className="w-5/6  mx-auto">
             <div className="h-[150px] flex flex-row gap-[31px]">
                 <img className="h-[250px] w-[250px] rounded-full border-solid border-[10px] border-white relative bottom-[100px]" referrerPolicy="no-referrer" src={profData.profileUrl} alt="" />
-                <div className="flex flex-col">
-                    <div>
-                        <p className="text-[34px] h-[36px] font-bold mt-[32px]">
-                            {profData.firstName ? profData.firstName : ""} {profData.lastName ? profData.lastName : ""}
-                        </p>
+                <div className="flex flex-col grow">
+                    <div className="text-[34px] h-[36px] font-bold mt-[32px]">
+                        { !editMode ? (
+                            <p>
+                                {(firstName ? firstName + " " : userName) + (firstName && lastName ? lastName : "")}
+                            </p>
+                        ) : (
+                            <div className="mb-[20px]">
+                                <input 
+                                    className = "w-[45%] border px-2 rounded mr-[0.5ch] p-0 outline-none inline-block text-gray-500"
+                                    placeholder='First Name' 
+                                    value={firstName} 
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                                <input 
+                                    className = "w-[45%] border px-2 rounded p-0 outline-none inline-block text-gray-500"
+                                    value={lastName}
+                                    placeholder='Last Name'
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </div>
+                            
+                        )}
                     </div>
                     <p className="text-[22px] h-[36px] font-semibold mt-[13px]"> Brown University </p>
                     <p className="text-[16px] text-gray-400">He/Him</p>
                 </div>
+                { editable ? (
+                    <div className="shrink flex">
+                        <button 
+                            className={`w-[186px] h-[50px] ${editMode ? "bg-[#ae2c27]" : "bg-[#b9b9b996]"} ${editMode ? "text-white" : "text-black"} rounded-lg text-[18px] mt-[32px] ml-auto font-bold`}
+                            onClick={() => editMode ? handleSave() : handleEdit()}
+                        >
+                            {editMode ? "Save" : "Edit Profile"}
+                        </button>
+                    </div>
+                ) : ( 
+                    null
+                )}
             </div>
-            <div className="ml-[5px] mt-[32px]">
+            <div className="{ml-[5px] mt-[32px]">
                 <p className="text-[22px] font-semibold">About</p>
-                <p className="text-[18px] mt-[11px]">{profData.bio}</p>
+                <div className={`text-[18px] ${editMode ? 'border rounded' : 'p-[5px]'} mt-[11px]`}>
+                    {editMode ? (
+                        <textarea 
+                            className = "w-[100%] p-[5px] rounded outline-none resize-none text-gray-500"
+                            value={bio}
+                            placeholder='Tell us about yourself...'
+                            onChange={(e) => setBio(e.target.value)}
+                        />
+                    ) : (
+                        <p>{bio}</p>
+                    )}
+                </div>
+                
             </div>
         </div>
         
